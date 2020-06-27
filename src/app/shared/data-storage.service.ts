b8dfa4a24da9be8a36edcdc8ddd9609124accf56
Erik <kiredroffas@aim.com>
2020-06-26 18:58:28 -0700

    Can now http GET recipes from firebase database and update interface (fetch data button in header)

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { RecipeService } from '../recipes/recipe.service';
+import { Recipe } from '../recipes/recipe.model';
 
 @Injectable({providedIn: 'root'})
 export class DataStorageService {
@@ -10,9 +11,18 @@ export class DataStorageService {
 
     storeRecipes() {
         const recipes = this.recipeService.getRecipes();
-        this.http.put('https://recipe-shopping-app-c70a7.firebaseio.com/recipes.json', recipes)
-        .subscribe(response => {
-            console.log(response);
-        });
+        this.http
+            .put('https://recipe-shopping-app-c70a7.firebaseio.com/recipes.json', recipes)
+            .subscribe(response => {
+                console.log(response);
+            });
+    }
+
+    fetchRecipes() {
+        this.http
+            .get<Recipe[]>('https://recipe-shopping-app-c70a7.firebaseio.com/recipes.json')
+            .subscribe(recipes => {
+                this.recipeService.setRecipes(recipes);
+            });
     }
 }
