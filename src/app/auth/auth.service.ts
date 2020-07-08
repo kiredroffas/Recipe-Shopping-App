58548cd80232272126afe8e7c990b64661de4fdd
Erik <kiredroffas@aim.com>
2020-07-08 16:59:01 -0700

    Adding the token to outgoing requests

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http'
 import { catchError, tap } from 'rxjs/operators';
-import { throwError, Subject } from 'rxjs';
+import { throwError, Subject, BehaviorSubject } from 'rxjs';
 import { User } from './user.model';
 
 export interface AuthResponseData {
@@ -16,7 +16,7 @@ export interface AuthResponseData {
 
 @Injectable({providedIn: 'root'})
 export class AuthService {
-    user = new Subject<User>();
+    user = new BehaviorSubject<User>(null);
 
     constructor(private http: HttpClient) {
 
@@ -65,7 +65,7 @@ export class AuthService {
 
     private handleAuthentication(email: string, userId: string, token: string, expiresIn: number) {
         const expirationDate = new Date(
-            new Date().getTime() + expiresIn * 1000
+            new Date().getTime() + (expiresIn * 1000)
         );
         const user = new User(
             email,
