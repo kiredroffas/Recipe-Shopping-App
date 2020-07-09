5b098a42e46cb8cc6541528851fe678453a34a1f
Erik <kiredroffas@aim.com>
2020-07-08 17:34:23 -0700

    Implemented auto-login if logged in user refreshes the page

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -64,6 +64,24 @@ export class AuthService {
          );
     }
 
+    // Retrieve logged in user if page is refreshed
+    autoLogin() {
+        const userData: {
+            email: string;
+            id: string;
+            _token: string;
+            _tokenExpirationDate: string;
+        } = JSON.parse(localStorage.getItem('userData'));
+        if(!userData) {
+            return;
+        }
+
+        const loadedUser = new User(userData.email, userData.id, userData._token, new Date(userData._tokenExpirationDate));
+        if(loadedUser.token) {
+            this.user.next(loadedUser);
+        }
+    }
+
     logout() {
         this.user.next(null);
         this.router.navigate(['/auth']);
@@ -80,6 +98,7 @@ export class AuthService {
             expirationDate
         );
         this.user.next(user);
+        localStorage.setItem('userData', JSON.stringify(user));
     }
 
     private handleError(errorRes: HttpErrorResponse) {
